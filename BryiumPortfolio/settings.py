7cd1cd463e8d5b12b5ebe718f8527a3b1720142c
Bryium <bryiumonyancha@gmail.com>
2024-08-04 20:29:51 +0300

    removed whitenoise

--- BryiumPortfolio/settings.py
+++ BryiumPortfolio/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,12 +131,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR / 'Home/static')
 ]
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- BryiumPortfolio/wsgi.py
+++ BryiumPortfolio/wsgi.py
@@ -19,6 +19,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BryiumPortfolio.settings')
 # Get the WSGI application for the project.
 application = get_wsgi_application()
 
-# Wrap the application with WhiteNoise for serving static files.
-application = WhiteNoise(application, root=str(BASE_DIR / 'staticfiles'))
-application.add_files(str(BASE_DIR / 'Home/static'), prefix='static/')
