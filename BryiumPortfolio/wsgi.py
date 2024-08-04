8487725fa99cd3f3dbe482835e62972a66a9d37a
Bryium <bryiumonyancha@gmail.com>
2024-08-04 14:30:32 +0300

    create teh wsgi application object

--- BryiumPortfolio/wsgi.py
+++ BryiumPortfolio/wsgi.py
@@ -12,6 +12,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BryiumPortfolio.settings')
 
+application = get_wsgi_application()
 try:
     application = get_wsgi_application()
 except Exception as e:
