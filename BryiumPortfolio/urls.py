13ed4bd5cb5b853628157710a3d8de0ddb693c99
Bryium <bryiumonyancha@gmail.com>
2024-08-04 19:23:07 +0300

    solved url

--- BryiumPortfolio/urls.py
+++ BryiumPortfolio/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from Home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('Home/', include('Home.urls')),
-    path('Home/<str:email>/', views.home, name='home_with_email'),
 ]
-
