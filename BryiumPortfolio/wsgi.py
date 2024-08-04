39daaeb0fba601d41fc21280344bfca941287f58
Bryium <bryiumonyancha@gmail.com>
2024-08-04 20:42:40 +0300

    solved the wsgi error

--- BryiumPortfolio/wsgi.py
+++ BryiumPortfolio/wsgi.py
@@ -10,17 +10,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-from BryiumPortfolio.settings import BASE_DIR
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BryiumPortfolio.settings')
 
 application = get_wsgi_application()
 
 app = application
 
-from whitenoise import WhiteNoise
-from BryiumPorfolio import MyWSGIApp
-
-application = MyWSGIApp()
-application = WhiteNoise(application, root=str(BASE_DIR / 'staticfiles'))
-application.add_files(str(BASE_DIR / 'Home/static'), prefix='static/')
