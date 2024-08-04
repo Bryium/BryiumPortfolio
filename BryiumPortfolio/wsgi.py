a88658e64a6e709de996eaef8e72aca7eeaf6e99
Bryium <bryiumonyancha@gmail.com>
2024-08-04 20:32:53 +0300

    changed the wsgi.py

--- BryiumPortfolio/wsgi.py
+++ BryiumPortfolio/wsgi.py
@@ -9,13 +9,18 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 from BryiumPortfolio.settings import BASE_DIR
 
-# Set the default settings module for the 'django' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BryiumPortfolio.settings')
 
-# Get the WSGI application for the project.
 application = get_wsgi_application()
 
+app = application
+
+from whitenoise import WhiteNoise
+from BryiumPorfolio import MyWSGIApp
+
+application = MyWSGIApp()
+application = WhiteNoise(application, root=str(BASE_DIR / 'staticfiles'))
+application.add_files(str(BASE_DIR / 'Home/static'), prefix='static/')
