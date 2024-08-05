09195e67f34a7938501a3d6d0bc0dbe6c8084520
Bryium <bryiumonyancha@gmail.com>
2024-08-05 03:31:42 +0300

    add the static urls

--- BryiumPortfolio/urls.py
+++ BryiumPortfolio/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from Home import views
 
 urlpatterns = [
@@ -7,3 +9,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('Home/', include('Home.urls')),
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
